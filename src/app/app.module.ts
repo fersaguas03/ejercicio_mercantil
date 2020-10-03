9667862751c7a92201be0da8d22c81d24e3683f5
Fer Saguas <fer.saguas03@gmail.com>
2020-10-02 21:31:23 -0300

    Creando componentes y armando plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
-import { BrowserModule } from '@angular/platform-browser';
+//ANGULAR
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
+//RUTAS
 import { AppRoutingModule } from './app-routing.module';
+
+//COMPONENTES
 import { AppComponent } from './app.component';
+
+//MODULOS
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import { ComponentsComponent } from './components/components.component';
+import { HomeComponent } from './pages/home/home.component';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import {PanelModule} from 'primeng/panel';
+
+
+import { TabViewModule } from 'primeng/tabview';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { ChipsModule } from 'primeng/chips';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+//import { DropdownModule } from 'primeng/dropdown';
+///import { MultiSelectModule } from 'primeng/multiselect';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    //MODULOS PrimeNg
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    InputSwitchModule,
+    PanelModule,
+    TabViewModule,
+    AutoCompleteModule,
+    CalendarModule,
+    ChipsModule,
+    InputMaskModule,
+    InputNumberModule,
+    InputTextareaModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
